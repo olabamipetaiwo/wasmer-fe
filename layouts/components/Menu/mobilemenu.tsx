22be4ae7eb75640f88fa816cde01778b85c21e40
olabamipe.taiwo <olabamipe.taiwo@mypatricia.co>
2023-05-22 12:47:13 +0100

    dev::mobiel nav

--- layouts/components/Menu/mobilemenu.tsx
+++ layouts/components/Menu/mobilemenu.tsx
@@ -8,15 +8,49 @@ import EdgeIcon from "@/assets/svg/edge.svg";
 import CLIcon from "@/assets/svg/cli.svg";
 import DocsIcon from "@/assets/svg/docs.svg";
 
+import SearchIcon from "@/assets/svg/search.svg";
+
 interface IProps {
   isOpened: boolean;
 }
 
 const MobileMenu = ({ isOpened }: IProps) => {
+  const mobileLinks = [
+    {
+      title: "Products",
+      link: "/Products",
+    },
+  ];
   return (
     <motion.nav className={` absolute top-44 z-20 w-full  `}>
-      <section className={`${styles.mobile__container} border p-10 flex`}>
-        <h2>this is mobilemenu</h2>
+      <section
+        className={`${styles.mobile__container} border p-10 flex flex-col`}
+      >
+        <div className={`flex items-center ${styles.nav__search}`}>
+          <SearchIcon />
+          <input type="text" placeholder="Search packages, users or apps" />
+        </div>
+
+        <ul className={`mt-5 ${styles.developer__links}`}>
+          {mobileLinks.map((_link) => {
+            return (
+              <li key={_link.title}>
+                <Link href="/">{_link.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+
+        <section className="flex items-center border border-t border-b border-black border-y py-8 my-8">
+          <CLIcon className="mr-2" />
+          <h2 className="developer-title text-black">CLI</h2>
+        </section>
+        <section className="flex flex-col align-start">
+          <p className="developer-headline text-left mb-4">Tools</p>
+          <h6 className="developer-title text-left">
+            Visual Studio Code Extension
+          </h6>
+        </section>
       </section>
     </motion.nav>
   );
